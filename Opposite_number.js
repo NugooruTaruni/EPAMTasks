0efc33f930e83be0cb6a2f91b2d57fff826751ad
Taruni <taruninugooru29@gmail.com>
2023-04-01 11:44:12 +0530

    first commit

---
+++ Opposite_number.js
@@ -0,0 +1,17 @@
+/*
+
+2) Opposite number.
+
+Very simple, given an integer or a floating-point number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(n)
+{
+    return -n;
+}
+console.log(opposite(786));
